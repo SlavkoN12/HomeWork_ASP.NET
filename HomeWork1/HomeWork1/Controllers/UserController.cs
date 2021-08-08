e76292329eb9f28aa0c9f9600d85b8c3fcb08ab9
Slavko Nikolovski <slavkonikolovski@hotmail.com>
2021-08-08 21:37:04 +0200

    homework

---
+++ HomeWork1/HomeWork1/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork1.Controllers
+{
+    public class UserController : Controller
+    {   
+        [HttpGet("/User")]
+        public IActionResult GetUser()
+        {
+            return View("User");
+        }
+
+        public IActionResult GetContact()
+        {
+            return RedirectToAction("Admin", "AdminController");
+
+        }
+        [HttpGet("/Empty")]
+        public IActionResult GetEmpty()
+        {
+            return new EmptyResult();
+        }
+
+        [HttpGet("/Id/{id}")]
+        public IActionResult GetId(int? id)
+        {
+            if (id.HasValue)
+            {
+                return View("Id");
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
